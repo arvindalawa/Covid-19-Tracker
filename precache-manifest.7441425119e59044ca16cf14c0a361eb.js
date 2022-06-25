df9d0a31db38680d1d36a96cfc550eb554276043
arvindalawa <102155525+arvindalawa@users.noreply.github.com>
2022-06-25 18:54:07 +0530

    Updates

---
+++ precache-manifest.7441425119e59044ca16cf14c0a361eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc277300c2456351281b7101946dda9",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "69b50061ad7316a79626",
+    "url": "/Covid-19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "bb479ab1c8a8bd517208",
+    "url": "/Covid-19-Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "69b50061ad7316a79626",
+    "url": "/Covid-19-Tracker/static/js/2.a1eacb32.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19-Tracker/static/js/2.a1eacb32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb479ab1c8a8bd517208",
+    "url": "/Covid-19-Tracker/static/js/main.8cc51796.chunk.js"
+  },
+  {
+    "revision": "4b644fa9ca0287604fad",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.b2c8fe1b.js"
+  }
+]);
